245d02717d766307cd630be83ae0c533a9b2efca
Sebastian Balderrama <capotillos11@gmail.com>
2024-11-28 00:25:29 -0700

    initial commit

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 16
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+-- config.window_background_opacity = 0.8
+-- config.macos_window_background_blur = 100
+
+config.colors = {
+	foreground = "#ffffff",
+	background = "#000000",
+}
+
+return config
