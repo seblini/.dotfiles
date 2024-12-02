3d8f130403f046bc3bfb9cbf0c970ff0cf166500
Sebastian Balderrama <capotillos11@gmail.com>
2024-12-01 20:39:39 -0700

    added remap to move selected text, added go support to lsp

--- nvim/lua/seblini/remap.lua
+++ nvim/lua/seblini/remap.lua
@@ -37,3 +37,6 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
