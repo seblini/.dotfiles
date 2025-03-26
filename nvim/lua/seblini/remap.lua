be4d6b4b3d24c02d30ba05b68c6cd2d13ba7189c
Sebastian Balderrama <capotillos11@gmail.com>
2025-03-25 23:52:36 -0600

    made small change to lspconfig.lua, added comment.lua and oil.lua, changed directory remap

--- nvim/lua/seblini/remap.lua
+++ nvim/lua/seblini/remap.lua
@@ -31,7 +31,8 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw" })
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw" })
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
